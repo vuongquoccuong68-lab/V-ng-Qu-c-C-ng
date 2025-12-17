2609d9a8bd52afe95700f635ea632dbefd14c19b
vuongquoccuong68-lab <vuongquoccuong68@gmail.com>
2025-12-17 15:47:20 +0700

    Update author information in app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Vương Quốc Cường - Trường Tiểu học Vĩnh Hảo, xã Vĩnh Tuy, tỉnh Tuyên Quang - ĐT: 0977 821 509</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Vương Quốc Cường - Trường Tiểu học Vĩnh Hảo, xã Vĩnh Tuy, tỉnh Tuyên Quang- ĐT: 0977 821 509</div>", unsafe_allow_html=True)
